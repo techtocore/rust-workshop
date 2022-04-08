daaed308d43718b8ec881b6059ce1b388a9385e2
Akash Ravi <akashkravi@gmail.com>
2022-04-07 21:50:16 -0400

    Records & displays scores

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,47 @@ struct MainState {
     y_vel: f32,
     top_paddle: Rect,
     bottom_paddle: Rect,
+    top_score: u32,
+    bottom_score: u32,
 }
 
 impl MainState {
 
     // since it accepts a &self parameter, draw() uses an immutable reference to a MainState instance
     fn draw(&self) {
-        draw_rectangle(self.ball.x, self.ball.y, self.ball.w, self.ball.h, WHITE);
+        draw_rectangle(self.ball.x, self.ball.y, self.ball.w, self.ball.h, YELLOW);
         draw_rectangle(self.top_paddle.x, self.top_paddle.y, self.top_paddle.w, self.top_paddle.h, RED);
         draw_rectangle(self.bottom_paddle.x, self.bottom_paddle.y, self.bottom_paddle.w, self.bottom_paddle.h, BLUE);
+
+        let t1: String = format!("{}{}", "Red Score: ", &self.top_score.to_string());
+        draw_text(&t1.to_owned(), screen_width() - 100.0, 40.0, 10.0, WHITE);
+        let t2: String = format!("{}{}", "Blue Score: ", &self.bottom_score.to_string());
+        draw_text(&t2.to_owned(), screen_width() - 100.0, 60.0, 10.0, WHITE);
     }
 
     fn update(&mut self) {
         self.ball.x += self.x_vel;
         self.ball.y += self.y_vel;
 
+        // Ball bouncing off edges
         if self.ball.right() >= screen_width() || self.ball.left() <= 0.0 {
             self.x_vel *= -1.0;
         }
-        if self.ball.bottom() >= screen_height() || self.ball.top() <= 0.0 {
+        if self.ball.bottom() >= screen_height() {
             self.y_vel *= -1.0;
+            self.top_score += 1;
+        }
+        if self.ball.top() <= 0.0 {
+            self.y_vel *= -1.0;
+            self.bottom_score += 1;
         }
 
+        // Ball bouncing off paddle
         if self.ball.overlaps(&self.top_paddle) || self.ball.overlaps(&self.bottom_paddle) {
             self.y_vel *= -1.0;
         }
 
+        // Keypress actions
         if is_key_down(KeyCode::Left) && self.bottom_paddle.x >= 0.0 {
             self.bottom_paddle.x -= 10.0
         }
@@ -55,6 +70,8 @@ async fn main() {
         y_vel: -5.0,
         top_paddle: Rect::new(screen_width() / 2.0 - 60.0, 15.0, 120.0, 15.0),
         bottom_paddle: Rect::new(screen_width() / 2.0 - 60.0, screen_height() - 30.0, 120.0, 15.0),
+        top_score: 0,
+        bottom_score: 0,
     };
 
     // equivalent to while(true)
